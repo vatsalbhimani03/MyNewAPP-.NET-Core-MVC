0465a7ced904e9974109ace6bc7b4592c0a9be24
Vatsal Bhimani <vbhimani@husky.ca>
2025-06-27 11:50:14 -0400

    Added basic project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyNewApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
