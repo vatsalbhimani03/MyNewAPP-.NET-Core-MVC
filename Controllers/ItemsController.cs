5085ad31cf6c2b79624b68ec1df76d2a59e380a9
Vatsal Bhimani <vbhimani@husky.ca>
2025-06-27 15:35:34 -0400

    added some controller and view add installed nuget packages

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyNewApp.Models;
+
+namespace MyNewApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        //http://localhost:5171/items/overview
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "Printer", Description = "It's a great printer"};
+            return View(item);
+        }
+
+          
+
+        //http://localhost:5171/items/edit?id= 100
+        public IActionResult Edit(int itemID)
+        {
+            return Content("id=" + itemID);
+        }
+
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace MyNewApp.Models
+{
+    public class Item
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Description { get; set; } 
+
+    }
+
+}
